9c954413d877b3e4d203056546414f166ae7ea5f
blurryface027 <krishna150487@gmail.com>
2026-03-17 19:25:40 +0530

    remove .next and ignore it

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++
@@ -1,1 +0,0 @@
-0:{"buildId":"1b5mUcEQYgwSi0T9tqQfH","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":false},"staleTime":300}
--- .next/server/chunks/ssr/_next-internal_server_app_(auth)_login_page_actions_a2babf1e.js
+++
@@ -1,3 +0,0 @@
-module.exports=[492625,a=>{"use strict";var b=a.i(330283);a.s([],637302),a.i(637302),a.s(["40a0c3f4add0723f1ee73a98e96713707b5ba0c50f",()=>b.signup,"40ce0392a00859b2f8da9518f0bf6f23872b3b5c02",()=>b.login],492625)}];
-
-//# sourceMappingURL=_next-internal_server_app_%28auth%29_login_page_actions_a2babf1e.js.map
