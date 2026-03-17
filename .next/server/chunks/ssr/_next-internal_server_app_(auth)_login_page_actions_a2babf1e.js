c322887c069f8340d1604fa7830edfccd180bca9
blurryface027 <krishna150487@gmail.com>
2026-03-17 18:57:28 +0530

    fix: middleware path issue

--- .next/server/chunks/ssr/_next-internal_server_app_(auth)_login_page_actions_a2babf1e.js
+++ .next/server/chunks/ssr/_next-internal_server_app_(auth)_login_page_actions_a2babf1e.js
@@ -1,3 +1,3 @@
-module.exports=[492625,a=>{"use strict";var b=a.i(330283);a.s([],637302),a.i(637302),a.s(["4033b65ce12c0f1c7f964febc619d89e5f05c995b8",()=>b.login,"409e7a413df3a675b4f558004c4e35a8adb8435af0",()=>b.signup],492625)}];
+module.exports=[492625,a=>{"use strict";var b=a.i(330283);a.s([],637302),a.i(637302),a.s(["40a0c3f4add0723f1ee73a98e96713707b5ba0c50f",()=>b.signup,"40ce0392a00859b2f8da9518f0bf6f23872b3b5c02",()=>b.login],492625)}];
 
 //# sourceMappingURL=_next-internal_server_app_%28auth%29_login_page_actions_a2babf1e.js.map
